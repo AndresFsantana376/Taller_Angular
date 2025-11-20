7e68a6c58cf636d7e285eb9f1f5a0f5cf3db0544
Andres Felipe Santana <af.santana@uniandes.edu.co>
2025-11-20 11:36:52 -0500

    commit final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
 import { provideHttpClient } from '@angular/common/http';
-=======
->>>>>>> fded70a795039cf287b6828003da198affa4b835
 
 import { routes } from './app.routes';
 
@@ -11,11 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-<<<<<<< HEAD
     provideRouter(routes),
     provideHttpClient() 
-=======
-    provideRouter(routes)
->>>>>>> fded70a795039cf287b6828003da198affa4b835
   ]
 };
